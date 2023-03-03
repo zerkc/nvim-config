4ad0cf84cfd9483cfa13f36ce2bb2568fb0b6344
zerkc <gustavo.a.g.latorre@gmail.com>
2023-03-03 01:03:21 -0300

    install file

---
+++ README.md
@@ -0,0 +1,5 @@
+#Single Configuration for neovim
+
+```bash
+bash -c "$(curl https://raw.githubusercontent.com/zerkc/nvim-config/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+TMPDIR=$(mktemp -d)
+
+git clone https://github.com/zerkc/nvim-config $TMPDIR
+
+mkdir -p ~/.config/nvim/
+
+mv $TMPDIR/* ~/.config/nvim/
+mv $TMPDIR/.* ~/.config/nvim/
+
+rm -rf $TMPDIR 
+
+cd ~/.config/nvim/ && ./config.sh
+
+echo "Successfully Install"
